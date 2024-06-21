d1b118a9b157a15b7c2ad8e0fc14e78226bd8461
ryunex15 <badarudin1511@gmail.com>
2024-06-21 17:16:32 +0700

    a

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getUsers } from "../controllers/UserController.js";
+import { getUsers,
+     getUserById,
+     createUser,
+     updateUser,
+     deleteUser
+    } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
