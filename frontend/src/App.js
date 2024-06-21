5d88beedc19772c6ffa0031a54016f6e9a48e869
ryunex15 <badarudin1511@gmail.com>
2024-06-21 19:14:06 +0700

    tambah add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
 
 function App() {
   return (
-    <div>
-      <UserList/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="add" element={<AddUser />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
